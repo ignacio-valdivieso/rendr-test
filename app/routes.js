001193114da12dd92e63750585880cbf6085efb9
ignacio-valdivieso <ignacio.valdivieso@olx.com>
2013-11-01 17:57:39 -0300

    fixed navigation and untracked files

--- app/routes.js
+++ app/routes.js
@@ -6,13 +6,20 @@ module.exports = function(match) {
   match('users/:login',       'users#show');
   match('users_lazy/:login',  'users#show_lazy');
 
-  if (global.isServer) {
-      match('/categories',     'categories#index');
-      match('/items',          'items#index');
-  } else {
-      match('categories?*params',     'categories#index');
-      match('items?*params',          'items#index');
-  }
+  // if (global.isServer) {
+  //     match('categories',     'categories#index');
+  //     match('items',          'items#index');
+  // } else {
+  // 	  match('categories',     'categories#index');
+  //     match('items',          'items#index');
+  //     match('categories?*params',     'categories#index');
+  //     match('items?*params',          'items#index');
+  // }
+
+  match('categories',     'categories#index');
+  match('items',          'items#index');
+  match('categories?*params',     'categories#index');
+  match('items?*params',          'items#index');
   
   //match('categories/:id?*params',	'categories#show');
   match('items/:id',				      'items#show');
